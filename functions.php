8d3064e226f6b8d28a951891d10a7075a70b55eb
Colin Frogner <Colin Frogner>
2016-05-30 17:47:18 -0700

    First Commit, creating something new from old

---
+++ functions.php
@@ -0,0 +1,16 @@
+<?php
+
+function streamerNames(){
+	$streamerNames = array(  "lirik",
+							"demolition_d",
+							"patrickklepek",
+							"drewscanlon",
+							"ltsquigs420",
+							"prewar0regon",
+							"thesnowballzz",
+						);
+	shuffle($streamerNames);
+	return $streamerNames;
+}
+
+?>
