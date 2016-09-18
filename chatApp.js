ac45a947cb7c7fa563f999e75a02c4a691fb20ca
Colin Frogner <Colin Frogner>
2016-09-17 17:38:15 -0700

    Combined random video page and chat page into one site

--- chatApp.js
+++ chatApp.js
@@ -9,18 +9,73 @@ var timeoutPlay = 0;
 var timeoutSeek = 0;
 var timeoutResume = 0;
 var timeoutFinish = 0;
+var dateTime = new Date().getTime() / 1000;
+var startTime = 1473904774;
+var globalTimeStamp = Math.floor(dateTime - startTime);
+var videoTimeStamp = 0;
+var totalLength = 0;
 
+console.log("Start Time: " + dateTime);
+console.log("Current Time: " + globalTimeStamp);
+console.log("Minutes: " + Math.floor(globalTimeStamp / 60));
+console.log("Seconds: " + Math.floor(globalTimeStamp % 60));
 
 // Serve up a video when page loads
-$(document).ready(pickRandomQuickLook);
+$(document).ready(calculateCurrentVideo());
+pickRandomQuickLook();
+setInterval(changeTime,1000);
 
 // Gets random video id and name from video array
 function pickRandomQuickLook(){
 	resetVideoTimeouts();
-	currentVideoIndex = Math.floor((Math.random() * 2169));
 	document.getElementById("currVideo").src='http://www.giantbomb.com/videos/embed/' + quickLooks[currentVideoIndex].split('~')[0] + '/';
 	document.getElementById("VideoTitle").innerHTML=quickLooks[currentVideoIndex].split('~')[1];
 	videoGBLink=quickLooks[currentVideoIndex].split('~')[2];
+}
+
+function changeTime(){
+	globalTimeStamp += 1;
+	var hours = Math.floor(globalTimeStamp / 3600);
+	var minutes = Math.floor((globalTimeStamp % 3600) / 60);
+	if(minutes < 10){
+		minutes = "0" + minutes;
+	}
+	
+	var seconds = Math.floor((globalTimeStamp % 3600) % 60);
+	if(seconds < 10){
+		seconds = "0" + seconds;
+	}
+	
+	if(hours > 0){
+		$('#timeSinceStart').html("Current Universal Time Stamp - " + hours + ":" + minutes + ":" + seconds);
+	}else{
+		$('#timeSinceStart').html("Current Universal Time Stamp - " + minutes + ":" + seconds);
+	}
+}
+
+function calculateCurrentVideo(){
+	var i = 0;
+	var lastLength = 0;
+	totalLength = 0;
+	
+	dateTime = new Date().getTime() / 1000;
+	globalTimeStamp = Math.floor(dateTime - startTime);
+	
+	//console.log("Before Calc: " + globalTimeStamp);
+	
+	for(i = 0; totalLength < globalTimeStamp; i++){
+		totalLength = totalLength + parseInt(quickLooks[i].split('~')[3]);
+		//console.log(quickLooks[i].split('~')[2]);
+		//console.log("Video Length: " + parseInt(quickLooks[i].split('~')[2]))
+		lastLength = parseInt(quickLooks[i].split('~')[3]);
+		//console.log(totalLength + " Index: " + i);
+	}
+	//console.log(totalLength);
+	//console.log(lastLength);
+	globalTimeStamp = lastLength - (totalLength - globalTimeStamp);
+	currentVideoIndex = i - 1;
+	//console.log(currentVideoIndex);
+	//console.log("After Calc: " + globalTimeStamp);
 }
 
 function resetVideoTimeouts(){
---
+++ videoApiControlGBR.js
@@ -0,0 +1,133 @@
+//Modified file from Giant Bomb API example
+
+var gVideoId = 0;
+var gApi = 0;
+var timeoutPlay = 0;
+var timeoutSeek = 0;
+var timeoutResume = 0;
+var timeoutFinish = 0;
+
+$(document).ready(
+
+	function ()
+	{
+	
+		$('iframe[data-cbsi-video]').each(
+
+			function(i,e)
+			{
+				e.addEventListener('load',
+
+					function()
+					{
+						var api = new CBSiVideoEmbedApi();
+						$.data(this, 'cbsi-embed-api', api);
+						
+						api.init(this,
+
+							function(result)
+							{
+								// The code is here is a hack, you need to make better...
+								if(i == 0){
+									gVideoId = result[0].id;
+									gApi = $.data(document.getElementById('currVideo'), 'cbsi-embed-api');
+								}
+								
+								setVideoForPlay();
+								
+							},
+
+							function(error)
+							{
+								alert('Oops! API returned error ' + error);
+							}
+						);
+					}
+				);
+			}
+		);
+	}
+);
+
+function setVideoForPlay(){
+	timeoutPlay = setTimeout('seekVideo(1)', 1000)
+	timeoutSeek = setTimeout(playVideo, 2000)
+	timeoutResume = setTimeout(resumeVideo, 4000)
+	timeoutFinish = setTimeout(alertWhenFinished, 10000);
+}
+
+function playVideo()
+{
+	gApi.play(gVideoId,
+
+		function()
+		{
+		},
+
+		function(error)
+		{
+			alert('Ooops! ' + error);
+		}
+	);
+}
+
+function pauseVideo()
+{
+	gApi.pause(gVideoId,
+
+		function()
+		{
+		},
+
+		function(error)
+		{
+			alert('Ooops! ' + error);
+		}
+	);
+}
+
+function resumeVideo()
+{
+	gApi.resume(gVideoId,
+
+		function()
+		{
+		},
+
+		function(error)
+		{
+			alert('Ooops! ' + error);
+		}
+	);
+}
+
+function seekVideo(timeStamp)
+{
+	gApi.setCurrentTimeStamp(gVideoId, timeStamp,
+
+		function()
+		{
+		},
+
+		function(error)
+		{
+			alert('Ooops! ' + error);
+		}
+	);
+}
+
+function alertWhenFinished()
+{
+	gApi.ended(gVideoId, true,
+
+		function()
+		{
+			pickRandomQuickLook();
+		},
+
+		function(error)
+		{
+			alert('Ooops! ' + error);
+		}
+	);
+}
